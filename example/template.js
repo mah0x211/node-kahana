656b92b1d1dab8fc63db2ae654a4822bf804d241
Masatoshi Teruya <mah0x211@gmail.com>
2011-01-31 17:51:09 +0900

    delete ClearSilver.renderFile method, change ClearSilver.renderString method

--- example/template.js
+++ example/template.js
@@ -1,14 +1,32 @@
 var util = require('util'),
+	fs = require('fs'),
 	Kahana = require('Kahana');
 
 var cs = new Kahana.ClearSilver;
-var text = "<?cs var:hello ?> <?cs var:world ?>";
-var hdf = { hello:'Hello', world:'World!' };
 
-cs.renderString( text, hdf, function( data ){
-console.log( 'renderString: ' + data );
-} );
+function Render( tmpl )
+{
+	var n = ( Number( process.argv[2] ) ) ? +process.argv[2] : 0;
+	var ms = +new Date();
+	
+	if( process.argv[3] ){
+		for( var i = 0; i < n; i++ ){
+			cs.renderString( tmpl, { items:['foo', 'bar', 'baz'] } );
+		}
+	}
+	else {
+		for( var i = 0; i < n; i++ ){
+			console.log( cs.renderString( tmpl, { items:['foo', 'bar', 'baz'] } ) );
+		}
+	}
+	console.log( +new Date() - ms + 'ms' + "\n" + util.inspect( process.memoryUsage() ) );	
+}
 
-cs.renderFile( __dirname + '/helloworld.txt', hdf, function( data ){
-	console.log( 'renderFile: ' + data );
+fs.readFile( __dirname + '/hello_cs.txt', function( err, data ){
+	if( err ){
+		throw err;
+	}
+	else {
+		Render( data.toString() );
+	}
 } );
